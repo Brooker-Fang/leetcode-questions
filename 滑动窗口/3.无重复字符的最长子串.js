73161e6dab443e45f2158ee04192e86285f7d82c
KingComedy <1455445725@qq.com>
2022-06-28 16:44:07 +0800

    optimization 3

--- 滑动窗口/3.无重复字符的最长子串.js
+++ 滑动窗口/3.无重复字符的最长子串.js
@@ -57,7 +57,10 @@
 /**
  * @param {string} s
  * @return {number}
- * 滑动窗口
+ * 滑动窗口思路：
+ *    右窗口边滑动边保存窗口里的字符 和 出现的次数，每次滑动保存 左窗口到右窗口的长度
+ *    当遇到window里有相同字符时，左窗口开始滑动
+ *    左窗口每次滑动则减少字符的次数，直到相同字符出现的次数 <= 1
  */
 var lengthOfLongestSubstring = function (s) {
   let right = 0,
@@ -66,15 +69,48 @@ var lengthOfLongestSubstring = function (s) {
   let len = 0;
   while (right < s.length) {
     let c = s[right];
-    right++;
+
     child[c] ? child[c]++ : (child[c] = 1);
     while (child[c] > 1) {
       let d = s[left];
       left++;
       child[d]--;
     }
-    len = Math.max(len, right - left);
+    len = Math.max(len, right - left + 1);
+
+    right++;
   }
   return len;
 };
+/* 
+  优化思路：
+    用map保存窗口内的字符和字符索引
+    当遇到相同字符时，将左窗口跳转到 之前保存字符的后面一个位置
+    注：要取左窗口索引 与 要跳转到的索引的 最大值, 因为左窗口跳转时，并没有清除跳转位置之前的字符与其索引，如
+*/
+var lengthOfLongestSubstring = function (s) {
+  const n = s.length;
+  if (n <= 1) return n;
+
+  let left = 0,
+    right = 0;
+  const window = new Map();
+  let maxLen = 0;
+  while (right < n) {
+    let c = s[right];
+    if (window.has(c)) {
+      left = Math.max(left, window.get(c) + 1);
+    }
+    maxLen = Math.max(maxLen, right - left + 1);
+    window.set(s[right], right);
+    right++;
+  }
+  console.info(maxLen);
+  return maxLen;
+};
+lengthOfLongestSubstring('abba');
+// lengthOfLongestSubstring("abcabcbb")
+// lengthOfLongestSubstring("bbbbb")
+// lengthOfLongestSubstring("pwwkew")
+// lengthOfLongestSubstring("dvdf")
 // @lc code=end
